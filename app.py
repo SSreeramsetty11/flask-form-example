a84fe436520ac81d2718fa9a6d2fe2f9243a5a8f
Saket Sreeramsetty <saketsreeramsetty11@gmail.com>
2025-01-20 18:56:48 -0800

    billy

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# List to store user data
+users = []
+
 @app.route('/')
 def home():
     return redirect(url_for('signup'))
@@ -12,14 +15,13 @@ def signup():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        # Here you would typically save the user data to a database
+        # Save user data to the list
+        users.append({'username': username, 'email': email})
         return redirect(url_for('data'))  # Redirect to a data page after sign up
     return render_template('signup.html')
 
 @app.route('/data')
 def data():
-    # Placeholder for user data
-    users = []  # Replace with actual user data retrieval
     return render_template('data.html', users=users)
 
 if __name__ == '__main__':
