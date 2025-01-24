73d861e616ccd945253824f146ddc6aef7e85cc7
Saket Sreeramsetty <saketsreeramsetty11@gmail.com>
2025-01-23 19:47:21 -0800

    fixed the jinja and bad key request errors

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # Specify the template folder
 
 # List to store user data
 users = []
 
 @app.route('/')
 def home():
-    return redirect(url_for('signup'))
+    return render_template('home.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        if username is None or email is None or password is None:
+            return 'Missing form data', 400
+        
         # Save user data to the list
         users.append({'username': username, 'email': email})
         return redirect(url_for('data'))  # Redirect to a data page after sign up
-    return render_template('signup.html')
+    else:
+        return render_template('signup.html')
 
 @app.route('/data')
 def data():
